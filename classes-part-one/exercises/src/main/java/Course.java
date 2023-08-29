458066cd01f0a818b378e5d82e819c6c8c5b3100
Natasha <nhodg2@gmail.com>
2023-08-28 19:42:00 -0500

    Update Course and Student Practice class

--- classes-part-one/exercises/src/main/java/Course.java
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Course {
+import java.util.ArrayList;
+public class Course {
+
+    private String title;
+    private Teacher instructor;
+    private int hours;
+    private ArrayList<Student> enrolledStudents = new ArrayList<>();
+
+    public Course(String title, ArrayList<Student> enrolledStudents, Teacher instructor, int hours) {
+        this.title = title;
+        this.enrolledStudents = enrolledStudents;
+        this.instructor = instructor;
+        this.hours = hours;
+    }
+
+    public Course(String title, ArrayList<Student> enrolledStudents) {
+        this("title", enrolledStudents, null, 0);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    void setTitle(String aTitle) {
+        title = aTitle;
+    }
+
+    public int getHours(){
+        return hours;
+    }
+
+    void setHours(int aHours){
+        hours = aHours;
+    }
+
+    public Teacher getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Teacher instructor) {
+        this.instructor = instructor;
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(ArrayList<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
 }
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,15 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student student = new Student("Natasha", 1234, 1, 4.0);
+        Teacher teacher = new Teacher("Shay", "Day", "English", 14);
+
+        System.out.println(student.getName());
+        System.out.println(student.getStudentId());
+        System.out.println(student.getGpa());
+        System.out.println(student.getNumberOfCredits());
+
+        System.out.println("\n" + teacher.getFirstName() + " " + teacher.getLastName() + " teaches " +
+                teacher.getSubject() + " and has been for " + teacher.getYearsTeaching() + " years.");
     }
 }
